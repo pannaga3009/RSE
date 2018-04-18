e4a3ca8c805d377b2f1ccd7a9cf22e880a3f061d
Ajay Ramesh <ajaycs14@gmail.com>
2018-04-18 15:12:07 -0500

    Small-Yelp-Dataset-JSON-Docs

---
+++ data/scripts/create_test_dataset.js
@@ -0,0 +1,48 @@
+/**
+ Data Cleaning Script 
+*/
+
+
+// Scenario 1: How to get the subset of data from yelp dataset ?
+
+// 1. Load the dataset JSON into mongodb
+// 2. Run below query
+
+use dataset;
+
+db.review.aggregate(
+   [ { $sample: { size: 100 } } ] // It will fetch 100 records
+)
+
+// 3. Export the query result to file
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+// Scenario 2: How to make Test dataset ?
+
+use dataset;
+db.review.aggregate(
+   [
+     { 
+       $group : { 
+                  _id : "$business_id",
+       		        reviews: { 
+                        $push: { 
+                          text: "$text", 
+                          ratings:"$stars",
+                          useful:"$useful" 
+                        } 
+                     } 
+                } 
+     },
+     { 
+       $limit: 10000 
+     }
+   ],
+    {
+      allowDiskUse: true
+    }
+)
+
